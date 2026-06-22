79b79973f895f5eeb623be92a961b692bab1cec2
elnmerahmed522-dot <elnmerahmed522@gmail.com>
2026-06-23 00:33:08 +0300

    fix: sanitize REQUEST_URI to handle Railway proxy CRLF injection

--- public/index.php
+++ public/index.php
@@ -5,15 +5,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Sanitize REQUEST_URI to prevent CR/LF/TAB injection from Railway's proxy
-if (isset($_SERVER['REQUEST_URI'])) {
-    $_SERVER['REQUEST_URI'] = str_replace(["\r", "\n", "\t"], '', $_SERVER['REQUEST_URI']);
-}
-if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $_SERVER['REMOTE_ADDR'] = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0];
-}
-
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
@@ -25,5 +16,13 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Sanitize server variables to remove CR/LF/TAB characters
+// This fixes a Railway proxy issue where CRLF characters appear in REQUEST_URI
+foreach (['REQUEST_URI', 'HTTP_HOST', 'QUERY_STRING', 'PATH_INFO'] as $key) {
+    if (isset($_SERVER[$key])) {
+        $_SERVER[$key] = str_replace(["\r", "\n", "\t"], '', $_SERVER[$key]);
+    }
+}
 
 $app->handleRequest(Request::capture());
