9582da559b872e078481ff6eb7af4027789cd751
elnmerahmed522-dot <elnmerahmed522@gmail.com>
2026-06-23 00:22:09 +0300

    fix: sanitize REQUEST_URI to strip CRLF from Railway proxy

--- public/index.php
+++ public/index.php
@@ -5,6 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Sanitize REQUEST_URI to prevent CR/LF/TAB injection from Railway's proxy
+if (isset($_SERVER['REQUEST_URI'])) {
+    $_SERVER['REQUEST_URI'] = str_replace(["\r", "\n", "\t"], '', $_SERVER['REQUEST_URI']);
+}
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $_SERVER['REMOTE_ADDR'] = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0];
+}
+
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
